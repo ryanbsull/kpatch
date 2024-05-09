ryanbsull/kpatch

commit 8b952bd77130bbac9f469ef76721557f7ea2f4ab

create-diff-object/ppc64le: Don't allow sibling calls
With the following patch:

diff --git a/net/sunrpc/xprtsock.c b/net/sunrpc/xprtsock.c
index e008aefc3a9d..7c70e369390d 100644
--- a/net/sunrpc/xprtsock.c
+++ b/net/sunrpc/xprtsock.c
@@ -2228,6 +2228,8 @@ static void xs_tcp_shutdown(struct rpc_xprt *xprt)
 	struct socket *sock = transport->sock;
 	int skst = transport->inet ? transport->inet->sk_state : TCP_CLOSE;

+	asm("nop");
+
 	if (sock == NULL)
 		return;
 	switch (skst) {

We saw the following panic on a RHEL7.6 kernel:

  Unable to handle kernel paging request for data at address 0xd00000000577f390
  Faulting instruction address: 0xd000000002e918f4
  Oops: Kernel access of bad area, sig: 11 [#1]
  SMP NR_CPUS=2048 NUMA pSeries
  Modules linked in: kpatch_3_10_0_957_1_3_1_1(OEK) nfsd nfs_acl rpcsec_gss_krb5 auth_rpcgss nfsv4 dns_resolver nfs lockd grace fscache sunrpc virtio_balloon ip_tables xfs libcrc32c virtio_net virtio_console virtio_blk virtio_pci virtio_ring virtio dm_mirror dm_region_hash dm_log dm_mod
  CPU: 9 PID: 5961 Comm: kworker/9:1H Kdump: loaded Tainted: G           OE K------------   3.10.0-957.1.3.el7.ppc64le #1
  Workqueue: xprtiod xprt_autoclose [sunrpc]
  task: c00000000300c3c0 ti: c0000003f1814000 task.ti: c0000003f1814000
  NIP: d000000002e918f4 LR: d000000002e57394 CTR: c00000000089d100
  REGS: c0000003f1817980 TRAP: 0300   Tainted: G           OE K------------    (3.10.0-957.1.3.el7.ppc64le)
  MSR: 8000000100009033 <SF,EE,ME,IR,DR,RI,LE>  CR: 240f2084  XER: 20000000
  CFAR: 000000010bb5270c DAR: d00000000577f390 DSISR: 40000000 SOFTE: 1
  GPR00: c00000000000b054 c0000003f1817c00 d00000000579add8 c000000214f0f4d0
  GPR04: c0000003fd618200 c0000003fd618200 0000000000000001 0000000000000dc2
  GPR08: 0000000000000dc3 0000000000000000 0000000000000000 d00000000577f370
  GPR12: c0000003f1814000 c000000007b85100 c00000000012fd88 c0000003f711bb40
  GPR16: 0000000000000000 0000000000000000 0000000000000000 0000000000000000

kpatch-build/create-diff-object.c
@@ -3253,6 +3253,60 @@ static void kpatch_build_strings_section_data(struct kpatch_elf *kelf)
 	}
 }
 
+/*
+ * Don't allow sibling calls from patched functions on ppc64le.  Before doing a
+ * sibling call, the patched function restores the stack to its caller's stack.
+ * The kernel-generated stub then writes the patch module's r2 (toc) value to
+ * the caller's stack, corrupting it, eventually causing a panic after it
+ * returns to the caller and the caller tries to use the livepatch module's toc
+ * value.
+ *
+ * In theory we could instead a) generate a custom stub, or b) modify the
+ * kernel livepatch_handler code to save/restore the stack r2 value, but this
+ * is easier for now.
+ */
+static void kpatch_no_sibling_calls_ppc64le(struct kpatch_elf *kelf)
+{
+#ifdef __powerpc64__
+	struct symbol *sym;
+	unsigned int insn;
+	unsigned long offset;
+
+	list_for_each_entry(sym, &kelf->symbols, list) {
+		if (sym->type != STT_FUNC || sym->status != CHANGED)
+			continue;
+
+		for (offset = 0; offset < sym->sec->data->d_size; offset += 4) {
+
+			insn = *(unsigned int *)(sym->sec->data->d_buf + offset);
+
+			/*
+			 * The instruction 0x48000000 can be assumed to be a
+			 * sibling call:
+			 *
+			 * Bits 0-5 (opcode) == 0x9: unconditional branch
+			 * Bit 30 (absolute) == 0: relative address
+			 * Bit 31 (link) == 0: doesn't set LR (not a call)
+			 *
+			 * Bits 6-29 (branch address) == zero, which means
+			 * it's either a branch to self (infinite loop), or
+			 * there's a REL24 relocation for the address which
+			 * will be written by the linker or the kernel.
+			 */
+			if (insn != 0x48000000)
+				continue;
+
+			/* Make sure it's not a branch-to-self: */
+			if (!find_rela_by_offset(sym->sec->rela, offset))
+				continue;
+
+			ERROR("Found an unsupported sibling call at %s()+0x%lx.  Add __attribute__((optimize(\"-fno-optimize-sibling-calls\"))) to %s() definition.",
+			      sym->name, sym->sym.st_value + offset, sym->name);
+		}
+	}
+#endif
+}
+
 struct arguments {
 	char *args[7];
 	int debug;
@@ -3413,6 +3467,8 @@ int main(int argc, char *argv[])
 	}
 	free(base_locals);
 	free(hint);
+
+	kpatch_no_sibling_calls_ppc64le(kelf_out);
 
 	/* create strings, patches, and dynrelas sections */
 	kpatch_create_strings_elements(kelf_out);

commit 08a6ad997b410e15dd4ae5b6240ed505b3e02058

kpatch-build: custom ppc TOC stub
Fixes kpatch sibling-call error by manually creating a TOC stub to be
branched to instead of a sibling call, this TOC stub recovers the TOC
from the initial module rather than saving the livepatch TOC to the
stack, which would corrupt the stack

Fixes: 8b952bd77130 (create-diff-object/ppc64le: Don't allow sibling calls)

Signed-off-by: Ryan Sullivan <rysulliv@redhat.com>

kmod/patch/ppc_stub.s
@@ -1,0 +1,10 @@
+.ppc_stub:
+	addis	11,2,-1
+	addi	11,11,26328
+	ld	12,32(11)
+	mtctr	12
+	ld	12, PACA_THREAD_INFO(13)
+	ld	11, TI_livepatch_sp(12)
+	ld	2,-24(11)
+	std	2,24(1)
+	b	32

kpatch-build/create-diff-object.c
@@ -34,6 +34,7 @@
  * the output object.
  */
 
+#include <libelf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -3895,12 +3896,63 @@ static void kpatch_build_strings_section_data(struct kpatch_elf *kelf)
  * kernel livepatch_handler code to save/restore the stack r2 value, but this
  * is easier for now.
  */
-static void kpatch_no_sibling_calls_ppc64le(struct kpatch_elf *kelf)
+
+static size_t get_toc_func(void* dest, char* src) {
+	Elf *elf = NULL;
+	Elf_Scn *scn = NULL;
+	Elf_Data *data = NULL;
+	Elf64_Shdr *shdr = NULL;
+	char *sec_name = NULL;
+	int fd;
+	size_t shstrndx = 0, data_size = 0;
+
+	elf_version(EV_CURRENT);
+
+	fd = open(src, O_RDONLY, 0);
+	elf = elf_begin(fd, ELF_C_READ, NULL);
+
+	elf_getshdrstrndx(elf, &shstrndx);
+
+	while ((scn = elf_nextscn(elf, scn)) != NULL) {
+		shdr = elf64_getshdr(scn);
+		sec_name = elf_strptr(elf, shstrndx, shdr->sh_name);
+		if (strcmp(sec_name, ".text") == 0) {
+			while ((data = elf_getdata(scn, data)) != NULL) {
+				memcpy(dest + data_size, data->d_buf, data->d_size);
+				data_size += data->d_size;
+			}
+		}
+	}
+	return data_size;
+}
+
+static struct symbol *generate_toc_stub(struct kpatch_elf *kelf, struct symbol *sym, unsigned int offset) {
+	struct symbol *toc_sym;
+	struct section *toc_sec;
+	void *toc_func;
+
+	if (!find_section_by_name(&kelf->sections, ".kpatch.ppc_stub"))
+		toc_sec = create_section_pair(kelf, ".kpatch.ppc_stub", 10, 4);
+	else
+		toc_sec = find_section_by_name(&kelf->sections, ".kpatch.ppc_stub");
+	toc_func = toc_sec->data->d_buf;
+	// putting in absolute value for now at root TODO: add to environment in build script
+	get_toc_func(toc_func, "/root/ppc_stub.o");
+	ALLOC_LINK(toc_sym, &kelf->symbols);
+	toc_sym->sec = toc_sec;
+	toc_sym->sym.st_info = GELF_ST_INFO(STB_LOCAL, STT_FUNC);
+	toc_sym->type = STT_FUNC;
+	toc_sym->bind = STB_LOCAL;
+	toc_sym->name = ".kpatch.ppc_stub";
+	return toc_sym;
+}
+
+static void kpatch_fix_sibling_calls_ppc64le(struct kpatch_elf *kelf)
 {
-	struct symbol *sym;
+	struct symbol *sym, *toc_sym;
 	unsigned char *insn;
 	unsigned int offset;
-	int sibling_call_errors = 0;
+	struct rela *ppc_rela, *orig, *repl = (struct rela*)malloc(sizeof(struct rela));
 
 	if (kelf->arch != PPC64)
 		return;
@@ -3934,15 +3986,33 @@ static void kpatch_no_sibling_calls_ppc64le(struct kpatch_elf *kelf)
 			if (!find_rela_by_offset(sym->sec->rela, offset))
 				continue;
 
-			log_normal("Found an unsupported sibling call at %s()+0x%lx.  Add __attribute__((optimize(\"-fno-optimize-sibling-calls\"))) to %s() definition.\n",
-			      sym->name, sym->sym.st_value + offset, sym->name);
-			sibling_call_errors++;
+			log_normal("Found sibling call at %s()+0x%lx.\n",
+			      sym->name, sym->sym.st_value + offset);
+			toc_sym = generate_toc_stub(kelf, sym, offset);
+			/* Create rela section entry where:
+			 * sib-call insn -> .kpatch.ppc_stub
+			 * rather than -> orig sib-call func
+			 */
+			list_for_each_entry(orig, &sym->sec->rela->relas, list) {
+				if (orig->offset == (sym->sym.st_value + offset)) {
+					break;
+				}
+			}
+			ALLOC_LINK(ppc_rela, &toc_sym->sec->relas);
+			ppc_rela->sym = orig->sym;
+			ppc_rela->rela = orig->rela;
+			ppc_rela->addend = 0;
+			ppc_rela->type = orig->type;
+			ppc_rela->type = absolute_rela_type(kelf);
+			ppc_rela->offset = (unsigned int)(32);
+			repl->sym = toc_sym;
+			repl->addend = orig->addend;
+			repl->offset = orig->offset;
+			repl->type = orig->type;
+			repl->rela = orig->rela;
+			list_replace(&orig->list, &repl->list);
 		}
 	}
-
-	if (sibling_call_errors)
-		ERROR("Found %d unsupported sibling call(s) in the patched code.",
-		      sibling_call_errors);
 }
 
 /* Check which functions have fentry/mcount calls; save this info for later use. */
@@ -4136,8 +4206,8 @@ int main(int argc, char *argv[])
 	 */
 	kpatch_elf_teardown(kelf_patched);
 
-	kpatch_no_sibling_calls_ppc64le(kelf_out);
-
+	/* Change sibling calls to regular calls */
+	kpatch_fix_sibling_calls_ppc64le(kelf_out);
 	/* create strings, patches, and klp relocation sections */
 	kpatch_create_strings_elements(kelf_out);
 	kpatch_create_patches_sections(kelf_out, lookup, parent_name);